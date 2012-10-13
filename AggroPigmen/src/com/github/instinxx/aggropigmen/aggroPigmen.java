b40a97ef3d6f9965ebc6729784888acfc6dbbb80
instinxx <smithtec@gmail.com>
2012-10-13 17:52:17 -0400

    rebuild using caffeine

--- AggroPigmen/src/com/github/instinxx/aggropigmen/aggroPigmen.java
+++ AggroPigmen/src/com/github/instinxx/aggropigmen/aggroPigmen.java
@@ -1,66 +1,58 @@
 package com.github.instinxx.aggropigmen;
 
-import org.bukkit.World;
-import org.bukkit.World.Environment;
+import java.util.logging.Logger;
+
+import org.bukkit.Server;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.PigZombie;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.plugin.java.JavaPlugin;
 
-
+@SuppressWarnings("unused")
 public class aggroPigmen extends JavaPlugin implements Listener {
+	public static aggroPigmen plugin;
+	public static final Logger log = Logger.getLogger("Minecraft");
+	
+	
+	@Override
 	public void onEnable() {
-		getLogger().info("AggroPigmen is loaded!");
+		plugin = this;
+		getLogger().info("AggroPigmen is Enabled!");
 		getServer().getPluginManager().registerEvents(this, this);
-	}		
+	}
 	
-	private Player target;		
-				
+	private Player target;
 	
-
+	
 	@EventHandler
 	public void onCreatureSpawn(CreatureSpawnEvent event){
-			
-			if(event.isCancelled()){
-				return;
-			}
-			
-			Entity ent = event.getEntity();
-			if (ent == null){
-				return;
-			}
-			
-			if (!(ent instanceof PigZombie)){
-				return;
-			}
-			
-			World world = null;
-			if (extracted(world).getEnvironment().equals(Environment.NETHER)){
-				return;
-			}
-			
-			else if (ent instanceof PigZombie){
-				PigZombie pz = (PigZombie)event.getEntity();
-				pz.setAngry(true);
-						
-				if (pz.getTarget() != target){
-					pz.setTarget(target);
-				}
-			}
-			
+
+		if(event.isCancelled()){
+			return;
 		}
 
-	private World extracted(World world) {
-		return world;
-	}
-		
+		Entity ent = event.getEntity();
+		if (ent == null){
+			return;
+		}
+
+		if (!(ent instanceof PigZombie)){
+			getLogger().info("AggroPigmen spawned!");
+			return;
+		}
+
+		else if (ent instanceof PigZombie){
+			PigZombie pz = (PigZombie)event.getEntity();
+			pz.setAngry(true);}
+		}
+			
+	
 	@Override
 	public void onDisable() {
-		// TODO Insert what happens on plugin Disable
-		getLogger().info("AggroPigmen are disabled!");
-	}
-}
-
+		getLogger().info("AggroPigmen are disabled - Be nice to them!");
+	}}
